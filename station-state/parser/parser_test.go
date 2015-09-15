1dbfc7eded494799a881a3e718ca127ef5612889
Alex Carol <alex.carol.c@gmail.com>
2015-09-15 18:59:47 +0200

    [WIP] Adding parser_test

--- station-state/parser/parser_test.go
+++ station-state/parser/parser_test.go
@@ -8,3 +8,33 @@ func TestXMLParsesReturnsErrWhenNotAssignedParsableXML(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestParseXMLReturnsAProperCollection(t *testing.T) {
+	var correctData = []byte(`<?xml version="1.0" encoding="UTF-8"?>
+    <bicing_stations>
+     <updatetime><![CDATA[1415996588]]></updatetime>
+      <station>
+        <id>1</id>
+          <type>BIKE</type>
+            <lat>41.397952</lat>
+              <long>2.180042</long>
+                <street><![CDATA[Gran Via Corts Catalanes]]></street>
+                  <height>21</height>
+                    <streetNumber>760</streetNumber>
+                      <nearbyStationList>24, 369, 387, 426</nearbyStationList>
+                        <status>OPN</status>
+                          <slots>0</slots>
+                            <bikes>24</bikes>
+                             </station>
+                             </bicing_stations>`)
+
+	_, err := ParseXML(correctData)
+	if err == nil {
+		t.FailNow()
+	}
+
+}
+
+func assertEquals(t *testing.T) {
+
+}
