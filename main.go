f4768b14843e7bcf8fc1162de094abbbe962730c
Alex Carol <alex.carol.c@gmail.com>
2016-02-04 09:01:25 +0100

    WIP

--- main.go
+++ main.go
@@ -1,28 +1,58 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"database/sql"
+	"fmt"
+	"io/ioutil"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/alexcarol/bicing-oracle/station-state/datasource"
-	"github.com/alexcarol/bicing-oracle/station-state/parser"
-	"github.com/alexcarol/bicing-oracle/station-state/repository"
 )
 
 func main() {
-	ticker := time.NewTicker(2 * time.Second)
-	quit := make(chan struct{})
+	db, dberr := sql.Open("mysql", "alex:alexpassword@/bicing_raw")
+	if dberr != nil {
+		panic(dberr)
+	}
 
-	storage := repository.NewStorage()
+	result, queryErr := db.Query("show databaes")
+	if queryErr != nil {
+		panic(queryErr)
+	}
+
+	fmt.Println(result)
+
+	fmt.Println("Starting querybicing")
+	ticker := time.NewTicker(45 * time.Second)
+
+	quit := make(chan struct{})
 
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				data, err := parser.ParseXML(datasource.FixtureData())
+				fmt.Println("Querying bicing")
+				var b bytes.Buffer
+				w := gzip.NewWriter(&b)
+
+				apiData, err := datasource.APIData()
 				if err != nil {
-					panic("Error parsing xml")
+					fmt.Println(err)
+					break
 				}
-				storage.PersistCollection(data)
+
+				w.Write(apiData)
+				w.Close()
+
+				err2 := ioutil.WriteFile(fmt.Sprintf("%d.xml.gz", int32(time.Now().Unix())), b.Bytes(), 0666)
+				if err2 != nil {
+					fmt.Println("Filesystem write went wrong")
+				}
+
 			case <-quit:
 				ticker.Stop()
 				return
---
+++ tools/fakemain.go
@@ -0,0 +1,34 @@
+package fakemain
+
+import (
+	"time"
+
+	"github.com/alexcarol/bicing-oracle/station-state/datasource"
+	"github.com/alexcarol/bicing-oracle/station-state/parser"
+	"github.com/alexcarol/bicing-oracle/station-state/repository"
+)
+
+func main() {
+	ticker := time.NewTicker(2 * time.Second)
+	quit := make(chan struct{})
+
+	storage := repository.NewStorage()
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				data, err := parser.ParseXML(datasource.FixtureData())
+				if err != nil {
+					panic("Error parsing xml")
+				}
+				storage.PersistCollection(data)
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	<-quit
+}
