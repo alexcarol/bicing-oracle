ba1f7f2d022e74ef9562ba11087eaf909d8a893a
Alex Carol <alex.carol.c@gmail.com>
2016-02-14 10:08:35 +0100

    Created checkup for bicing updates

--- api/api.go
+++ api/api.go
@@ -2,34 +2,56 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
-
-	"encoding/json"
-
-	"github.com/alexcarol/bicing-oracle/station-state/datasource"
-	"github.com/alexcarol/bicing-oracle/station-state/parser"
+	"os"
+	"database/sql"
+	"time"
+	_ "github.com/alexcarol/bicing-oracle/Godeps/_workspace/src/github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	dbName := getEnv("MYSQL_RAW_DATA_NAME", "bicing_raw")
 
-	http.HandleFunc("/stations", getAllStations)
+	username := getEnv("MYSQL_RAW_DATA_USER", "root")
+	password := getEnv("MYSQL_RAW_DATA_PASSWORD", "")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func getAllStations(w http.ResponseWriter, r *http.Request) {
-	apiData := datasource.FixtureData()
-
-	stationStateCollection, error := parser.ParseXML(apiData)
-	if error != nil {
-		fmt.Fprintf(w, "Error executing %q", html.EscapeString(r.URL.Path))
-		fmt.Print(error)
+	port := getEnv("MYSQL_RAW_DATA_ADDRESS", "localhost:3306")
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, port, dbName))
+	if err != nil {
+		panic(err)
 	}
 
-	json.NewEncoder(w).Encode(stationStateCollection)
+	http.HandleFunc("/checkup", func(w http.ResponseWriter, r *http.Request) {
+		var updatetime int64
+		err := db.QueryRow("select UNIX_TIMESTAMP(last_updatetime) from station order by last_updatetime desc limit 1").Scan(&updatetime);
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+
+			return;
+		}
+
+		currentTime := time.Now().Unix()
+		twoMinutesInThePast := currentTime - 120
+		if twoMinutesInThePast > updatetime {
+			http.Error(w, fmt.Sprintf("Time difference too big, current time : %d, update time: %d, difference: %d", currentTime, updatetime, currentTime - updatetime), 500)
+
+			return;
+		} else {
+			fmt.Fprintf(w, "Time difference is reasonable, current time : %d, update time: %d, difference: %d", currentTime, updatetime, currentTime - updatetime)
+
+			return;
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func getEnv(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	return value
 }
