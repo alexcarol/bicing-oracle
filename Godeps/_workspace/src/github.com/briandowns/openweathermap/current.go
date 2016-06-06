53a616cdb9c75348109959908f58cb2ab66f3e8b
Alex Carol <alex.carol.c@gmail.com>
2016-06-06 19:33:18 +0200

    Updated openweathermap

--- Godeps/_workspace/src/github.com/briandowns/openweathermap/current.go
+++ Godeps/_workspace/src/github.com/briandowns/openweathermap/current.go
@@ -25,17 +25,19 @@ import (
 // CurrentWeatherData struct contains an aggregate view of the structs
 // defined above for JSON to be unmarshaled into.
 type CurrentWeatherData struct {
-	GeoPos  Coordinates `json:"coord"`
-	Sys     Sys         `json:"sys"`
-	Base    string      `json:"base"`
-	Weather []Weather   `json:"weather"`
-	Main    Main        `json:"main"`
-	Wind    Wind        `json:"wind"`
-	Clouds  Clouds      `json:"clouds"`
-	Dt      int         `json:"dt"`
-	ID      int         `json:"id"`
-	Name    string      `json:"name"`
-	Cod     int         `json:"cod"`
+	GeoPos  Coordinates        `json:"coord"`
+	Sys     Sys                `json:"sys"`
+	Base    string             `json:"base"`
+	Weather []Weather          `json:"weather"`
+	Main    Main               `json:"main"`
+	Wind    Wind               `json:"wind"`
+	Clouds  Clouds             `json:"clouds"`
+	Rain    map[string]float64 `json:"rain"`
+	Snow    map[string]float64 `json:"snow"`
+	Dt      int                `json:"dt"`
+	ID      int                `json:"id"`
+	Name    string             `json:"name"`
+	Cod     int                `json:"cod"`
 	Unit    string
 	Lang    string
 	Key     string
@@ -63,18 +65,6 @@ func NewCurrent(unit, lang string) (*CurrentWeatherData, error) {
 	c.Key = getKey()
 
 	return c, nil
-}
-
-// SetLang allows you to set the language responses will be displayed as.  This isn't part of the
-// NewCurrent call because it'd keep it easier to go with API defaults and
-// adjust if explicitly called.
-func (w *CurrentWeatherData) SetLang(lang string) error {
-	if !ValidLangCode(lang) {
-		return errLangUnavailable
-	}
-	w.Lang = lang
-
-	return nil
 }
 
 // CurrentByName will provide the current weather with the provided
