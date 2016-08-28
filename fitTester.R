9fd55f9a27abf378247f06d0710cf641aaceb81d
Alex Carol <alex.carol.c@gmail.com>
2016-08-28 12:40:12 +0200

    Fixed fitTester.R again

--- fitTester.R
+++ fitTester.R
@@ -30,7 +30,7 @@ fit <- readRDS(objectID)
 
 data$prediction <- predict(fit, data)
 
-data$predictionOk <- data$pbikes == 1 & data$prediction > 0.5 | data$pbikes == 0 & data$prediction <= 0.
+data$predictionOk <- data$pbikes == 1 & data$prediction > 0.5 | data$pbikes == 0 & data$prediction <= 0.5
 
 table(data$predictionOk, data$pbikes)
 
