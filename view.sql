1b335fc24cac1df4d5937e5afdc764e8ee874976
Alex Carol <alex.carol.c@gmail.com>
2016-08-30 13:32:30 +0200

    Added fit precalculation 2

--- view.sql
+++ view.sql
@@ -1,7 +1,18 @@
 DROP VIEW fit_precalculation;
 
 CREATE VIEW fit_precalculation
-AS SELECT s.id, s.updatetime, s.bikes > 1 as pbikes, s.slots < 1 as pslots, w.type as weather_type, w.temperature
+AS SELECT s.id, s.updatetime, s.bikes >= 1 as pbikes, s.slots >= 1 as pslots, w.type as weather_type, w.temperature
+    FROM station_state s, weather w
+    WHERE w.time = (SELECT w2.time
+    FROM weather w2
+    WHERE w2.time >= s.updatetime
+    ORDER BY w2.time ASC
+    LIMIT 1
+);
+DROP VIEW fit_precalculation_2;
+
+CREATE VIEW fit_precalculation_2
+AS SELECT s.id, s.bikes, s.slots, s.updatetime, s.bikes >= 1 as pbikes, s.slots >= 1 as pslots, w.type as weather_type, w.temperature
     FROM station_state s, weather w
     WHERE w.time = (SELECT w2.time
     FROM weather w2
